93a7f6827dffc8d66ee83594a28ac71efc5c352b
matt <matt@cdmdotnet.com>
2014-01-17 21:52:21 +0000

    Added mongo implementation of ISingleSignOnToken
    git-svn-id: file:///data/svn/Cqrs/trunk@53 72444466-9f5c-4d9b-9c6f-db583910f96c

---
+++ Cqrs/Framework/Cqrs.Mongo/Repositories/Authentication/SingleSignOnToken.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using Cqrs.Mongo.Entities;
+using Cqrs.Repositories.Authentication;
+
+namespace Cqrs.Mongo.Repositories.Authentication
+{
+	[Serializable]
+	[DataContract]
+	public class SingleSignOnToken : MongoEntity, ISingleSignOnToken
+	{
+		[Required]
+		[DataMember]
+		public string Token { get; set; }
+
+		[Required]
+		[DataMember]
+		public DateTime TimeOfExpiry { get; set; }
+
+		[Required]
+		[DataMember]
+		public DateTime DateIssued { get; set; }
+	}
+}
