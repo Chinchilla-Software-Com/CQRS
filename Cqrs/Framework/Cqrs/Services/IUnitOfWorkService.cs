82df2bc8dd17a1a3c89cfdc7ce2d5096fa4424cf
matt <matt@cdmdotnet.com>
2014-05-15 09:21:37 +0000

    Added IUnitOfWorkService interface
    git-svn-id: file:///data/svn/Cqrs/trunk@87 72444466-9f5c-4d9b-9c6f-db583910f96c

---
+++ Cqrs/Framework/Cqrs/Services/IUnitOfWorkService.cs
@@ -0,0 +1,21 @@
+﻿namespace Cqrs.Services
+{
+	public interface IUnitOfWorkService
+	{
+		/// <summary>
+		/// Informs the service of the object that will be committing the UnitOfWork.
+		/// </summary>
+		/// <returns>
+		/// true if the provided <paramref name="commiter"/> is accepted as the committer, false otherwise.
+		/// </returns>
+		bool SetCommitter(object commiter);
+
+		/// <summary>
+		/// Commits the UnitOfWork if the provided <paramref name="commiter"/> is the Committer.
+		/// </summary>
+		/// <returns>
+		/// true if the provided <paramref name="commiter"/> is the Committer, false otherwise.
+		/// </returns>
+		bool Commit(object commiter);
+	}
+}
