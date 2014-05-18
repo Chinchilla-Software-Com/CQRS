7861a13686f427fba2f4debfe8df0e3161fd8fa1
matt <matt@cdmdotnet.com>
2014-05-18 05:06:58 +0000

    Added the MongoIndex to the project.
    git-svn-id: file:///data/svn/Cqrs/trunk@103 72444466-9f5c-4d9b-9c6f-db583910f96c

---
+++ Cqrs/Framework/Cqrs.Mongo/DataStores/MongoIndex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cqrs.Mongo.DataStores
+{
+	public abstract class MongoIndex<TEntity>
+	{
+		/// <summary>
+		/// Indicates if the index enforces unique values. Defaults to true.
+		/// </summary>
+		public bool IsUnique { get; protected set; }
+
+		/// <summary>
+		/// Indicates if the index is in ascending order or descending. Defaults to true meaning ascending order.
+		/// </summary>
+		public bool IsAcending { get; protected set; }
+
+		/// <summary>
+		/// The name of the index. Default to the class name removing any instances of "Index" and "MongoIndex" from the name.
+		/// </summary>
+		public string Name { get; protected set; }
+
+		public IEnumerable<System.Linq.Expressions.Expression<Func<TEntity, object>>> Selectors { get; protected set; }
+
+		protected MongoIndex()
+		{
+			IsUnique = true;
+			IsAcending = true;
+			Name = GetType()
+				.Name
+				.Replace("Index", string.Empty)
+				.Replace("MongoIndex", string.Empty);
+		}
+	}
+}
