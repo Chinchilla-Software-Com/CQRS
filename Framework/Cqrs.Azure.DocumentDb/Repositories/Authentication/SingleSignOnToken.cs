766584c5067f6906d1a7520ca1f12912fb00b5b5
grover <grover@cdmdotnet.com>
2014-10-08 10:44:11 +0000

    Added a SingleSignOnToken
    git-svn-id: file:///data/svn/Cqrs/trunk@190 72444466-9f5c-4d9b-9c6f-db583910f96c

---
+++ Framework/Cqrs.Azure.DocumentDb/Repositories/Authentication/SingleSignOnToken.cs
@@ -0,0 +1,32 @@
+﻿#region Copyright
+// // -----------------------------------------------------------------------
+// // <copyright company="cdmdotnet Limited">
+// // 	Copyright cdmdotnet Limited. All rights reserved.
+// // </copyright>
+// // -----------------------------------------------------------------------
+#endregion
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using Cqrs.Authentication;
+
+namespace Cqrs.Azure.DocumentDb.Repositories.Authentication
+{
+	[Serializable]
+	[DataContract]
+	public class SingleSignOnToken : ISingleSignOnToken
+	{
+		[Required]
+		[DataMember]
+		public string Token { get; set; }
+
+		[Required]
+		[DataMember]
+		public DateTime TimeOfExpiry { get; set; }
+
+		[Required]
+		[DataMember]
+		public DateTime DateIssued { get; set; }
+	}
+}
