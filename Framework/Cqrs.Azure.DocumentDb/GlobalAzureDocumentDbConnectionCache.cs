21f3bb6cbcda28780e26388818922be8cefc2c92
grover <grover@cdmdotnet.com>
2014-10-13 07:47:37 +0000

    Added a global cache for the Azure DocumentDB client and connection.
    git-svn-id: file:///data/svn/Cqrs/trunk@216 72444466-9f5c-4d9b-9c6f-db583910f96c

---
+++ Framework/Cqrs.Azure.DocumentDb/GlobalAzureDocumentDbConnectionCache.cs
@@ -0,0 +1,66 @@
+﻿#region Copyright
+// // -----------------------------------------------------------------------
+// // <copyright company="cdmdotnet Limited">
+// // 	Copyright cdmdotnet Limited. All rights reserved.
+// // </copyright>
+// // -----------------------------------------------------------------------
+#endregion
+
+using System.Collections.Generic;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+namespace Cqrs.Azure.DocumentDb
+{
+	public class GlobalAzureDocumentDbConnectionCache : IAzureDocumentDbConnectionCache
+	{
+		protected static IDictionary<string, object> Cache { get; private set; }
+
+		static GlobalAzureDocumentDbConnectionCache()
+		{
+			Cache = new Dictionary<string, object>();
+		}
+
+		public bool TryGetClient(string key, out DocumentClient client)
+		{
+			try
+			{
+				object cacheResult;
+				bool result = Cache.TryGetValue(key, out cacheResult);
+				client = cacheResult as DocumentClient;
+				return result && client != null;
+			}
+			catch
+			{
+			}
+			client = null;
+			return false;
+		}
+
+		public void SetClient(string key, DocumentClient client)
+		{
+			Cache[key] = client;
+		}
+
+		public bool TryGetDatabase(string key, out Database database)
+		{
+			try
+			{
+				object cacheResult;
+				bool result = Cache.TryGetValue(key, out cacheResult);
+				database = cacheResult as Database;
+				return result && database != null;
+			}
+			catch
+			{
+			}
+			database = null;
+			return false;
+		}
+
+		public void SetDatabase(string key, Database database)
+		{
+			Cache[key] = database;
+		}
+	}
+}
