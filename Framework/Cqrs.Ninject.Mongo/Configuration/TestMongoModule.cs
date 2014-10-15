c5560de5bbe52c331334181a3d5bbef20816b7fd
grover <grover@cdmdotnet.com>
2014-10-15 21:19:54 +0000

    Added a Test Mongo Niject module.
    git-svn-id: file:///data/svn/Cqrs/trunk@226 72444466-9f5c-4d9b-9c6f-db583910f96c

---
+++ Framework/Cqrs.Ninject.Mongo/Configuration/TestMongoModule.cs
@@ -0,0 +1,29 @@
+﻿#region Copyright
+// // -----------------------------------------------------------------------
+// // <copyright company="cdmdotnet Limited">
+// // 	Copyright cdmdotnet Limited. All rights reserved.
+// // </copyright>
+// // -----------------------------------------------------------------------
+#endregion
+
+using Cqrs.Mongo.Factories;
+
+namespace Cqrs.Ninject.Mongo.Configuration
+{
+	/// <summary>
+	/// A <see cref="MongoModule"/> that assumes the connection string is keyed "MongoDb-Test".
+	/// This will generated a random suffix to the database name so as to create a unique store for testing against.
+	/// </summary>
+	public class TestMongoModule : MongoModule
+	{
+		/// <summary>
+		/// Register the all factories
+		/// </summary>
+		public override void RegisterFactories()
+		{
+			Bind<IMongoDataStoreConnectionStringFactory>()
+				.To<TestMongoDataStoreConnectionStringFactory>()
+				.InSingletonScope();
+		}
+	}
+}
---
+++ Framework/Cqrs.Ninject.Mongo/TestMongoDataStoreConnectionStringFactory.cs
@@ -0,0 +1,43 @@
+﻿#region Copyright
+// // -----------------------------------------------------------------------
+// // <copyright company="cdmdotnet Limited">
+// // 	Copyright cdmdotnet Limited. All rights reserved.
+// // </copyright>
+// // -----------------------------------------------------------------------
+#endregion
+
+using System.Configuration;
+using System.Runtime.Remoting.Messaging;
+using Cqrs.Mongo.Factories;
+
+namespace Cqrs.Ninject.Mongo
+{
+	public class TestMongoDataStoreConnectionStringFactory : IMongoDataStoreConnectionStringFactory
+	{
+		private const string MongoDbConnectionStringKey = "MongoDb-Test";
+
+		private const string CallContextDatabaseNameKey = "MongoDataStoreConnectionStringFactory¿DatabaseName";
+
+		public static string DatabaseName
+		{
+			get
+			{
+				return (string)CallContext.GetData(CallContextDatabaseNameKey);
+			}
+			set
+			{
+				CallContext.SetData(CallContextDatabaseNameKey, value);
+			}
+		}
+
+		public string GetMongoConnectionString()
+		{
+			return ConfigurationManager.ConnectionStrings[MongoDbConnectionStringKey].ConnectionString;
+		}
+
+		public string GetMongoDatabaseName()
+		{
+			return DatabaseName;
+		}
+	}
+}
