2f3774d937a7032ad07acc0b22499fee4a840594
grover <grover@cdmdotnet.com>
2015-03-26 07:29:43 +0000

    Added more constructors
    git-svn-id: file:///data/svn/Cqrs/trunk@310 72444466-9f5c-4d9b-9c6f-db583910f96c

--- Framework/Cqrs/Domain/Factories/AggregateFactory.cs
+++ Framework/Cqrs/Domain/Factories/AggregateFactory.cs
@@ -1,19 +1,45 @@
 ﻿using System;
+using System.Reflection;
+using Cqrs.Configuration;
 using Cqrs.Domain.Exception;
+using Cqrs.Logging;
 
 namespace Cqrs.Domain.Factories
 {
 	public class AggregateFactory : IAggregateFactory
 	{
+		protected IDependencyResolver DependencyResolver { get; private set; }
+
+		public AggregateFactory(IDependencyResolver dependencyResolver)
+		{
+			DependencyResolver = dependencyResolver;
+		}
+
 		public TAggregate CreateAggregate<TAggregate>()
 		{
 			try
 			{
-				return (TAggregate)Activator.CreateInstance(typeof(TAggregate), true);
+				DependencyResolver.Resolve<TAggregate>();
 			}
-			catch (MissingMethodException)
+			catch (System.Exception dependencyResolverException)
 			{
-				throw new MissingParameterLessConstructorException(typeof(TAggregate));
+				DependencyResolver.Resolve<ILog>().LogDebug("Using the dependency resolver to create an instance of the aggregate failed.", "Cqrs.Domain.Factories.AggregateFactory.CreateAggregate", dependencyResolverException);
+				try
+				{
+					Activator.CreateInstance(typeof(TAggregate), BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.CreateInstance, null, new object[] { DependencyResolver, DependencyResolver.Resolve<ILog>() }, null);
+				}
+				catch (MissingMethodException exception)
+				{
+					DependencyResolver.Resolve<ILog>().LogDebug("Looking for a private constructor with a dependency resolver and logger, to create an instance of the aggregate failed.", "Cqrs.Domain.Factories.AggregateFactory.CreateAggregate", exception);
+					try
+					{
+						return (TAggregate)Activator.CreateInstance(typeof(TAggregate), true);
+					}
+					catch (MissingMethodException)
+					{
+						throw new MissingParameterLessConstructorException(typeof(TAggregate));
+					}
+				}
 			}
 		}
 	}
