990ce8f63e4308565712dcb2589c83ad235832b1
grover <grover@cdmdotnet.com>
2015-03-28 03:32:00 +0000

    Added test modules
    git-svn-id: file:///data/svn/Cqrs/trunk@325 72444466-9f5c-4d9b-9c6f-db583910f96c

---
+++ Framework/Cqrs.Ninject.Azure.DocumentDb/Configuration/TestAzureDocumentDbEventStoreModule.cs
@@ -0,0 +1,82 @@
+﻿#region Copyright
+// // -----------------------------------------------------------------------
+// // <copyright company="cdmdotnet Limited">
+// // 	Copyright cdmdotnet Limited. All rights reserved.
+// // </copyright>
+// // -----------------------------------------------------------------------
+#endregion
+
+using System.Linq;
+using Cqrs.Azure.DocumentDb;
+using Cqrs.Azure.DocumentDb.Events;
+using Cqrs.Events;
+using Cqrs.Ninject.Azure.DocumentDb.Events;
+using Ninject.Modules;
+
+namespace Cqrs.Ninject.Azure.DocumentDb.Configuration
+{
+	/// <summary>
+	/// The <see cref="INinjectModule"/> for use with the Cqrs package.
+	/// </summary>
+	public class TestAzureDocumentDbEventStoreModule<TAuthenticationToken> : NinjectModule
+	{
+		#region Overrides of NinjectModule
+
+		/// <summary>
+		/// Loads the module into the kernel.
+		/// </summary>
+		public override void Load()
+		{
+			RegisterFactories();
+			RegisterServices();
+			RegisterCqrsRequirements();
+			RegisterAzureHelpers();
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Register the all factories
+		/// </summary>
+		public virtual void RegisterFactories()
+		{
+			Bind<IEventBuilder<TAuthenticationToken>>()
+				.To<AzureDocumentDbEventBuilder<TAuthenticationToken>>()
+				.InSingletonScope();
+			Bind<IEventDeserialiser<TAuthenticationToken>>()
+				.To<AzureDocumentDbEventDeserialiser<TAuthenticationToken>>()
+				.InSingletonScope();
+		}
+
+		/// <summary>
+		/// Register the all services
+		/// </summary>
+		public virtual void RegisterServices()
+		{
+		}
+
+		public virtual void RegisterAzureHelpers()
+		{
+			if (!Kernel.GetBindings(typeof(IAzureDocumentDbHelper)).Any())
+			{
+				Bind<IAzureDocumentDbHelper>()
+					.To<AzureDocumentDbHelper>()
+					.InSingletonScope();
+			}
+		}
+
+		/// <summary>
+		/// Register the all Cqrs command handlers
+		/// </summary>
+		public virtual void RegisterCqrsRequirements()
+		{
+			Bind<IAzureDocumentDbEventStoreConnectionHelper>()
+				.To<TestAzureDocumentDbEventStoreConnectionHelper>()
+				.InSingletonScope();
+
+			Bind<IEventStore<TAuthenticationToken>>()
+				.To<AzureDocumentDbEventStore<TAuthenticationToken>>()
+				.InSingletonScope();
+		}
+	}
+}
