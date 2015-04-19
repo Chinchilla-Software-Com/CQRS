7316011230d9bbe3671aed11216c166b3a8852da
grover <grover@cdmdotnet.com>
2015-04-19 08:03:51 +0000

    Added a default implementation of SingleSignOnToken
    git-svn-id: file:///data/svn/Cqrs/trunk@392 72444466-9f5c-4d9b-9c6f-db583910f96c

---
+++ Framework/Cqrs/Repositories/Authentication/SingleSignOnToken.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using Cqrs.Authentication;
+
+namespace Cqrs.Repositories.Authentication
+{
+	[Serializable]
+	[DataContract]
+	public class SingleSignOnToken : ISingleSignOnToken
+	{
+		[Required]
+		[DataMember]
+		public string Token { get; set; }
+
+		[Required]
+		[DataMember]
+		public DateTime TimeOfExpiry { get; set; }
+
+		[Required]
+		[DataMember]
+		public DateTime DateIssued { get; set; }
+	}
+}
