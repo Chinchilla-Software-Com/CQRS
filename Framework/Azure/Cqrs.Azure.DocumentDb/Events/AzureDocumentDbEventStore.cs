08401a5a53c13e62665832d59ab1796e61e6e798
grover <grover@cdmdotnet.com>
2015-04-22 11:32:30 +0000

    Trialing a fix for azure documentdb eventstore.
    git-svn-id: file:///data/svn/Cqrs/trunk@420 72444466-9f5c-4d9b-9c6f-db583910f96c

--- Framework/Azure/Cqrs.Azure.DocumentDb/Events/AzureDocumentDbEventStore.cs
+++ Framework/Azure/Cqrs.Azure.DocumentDb/Events/AzureDocumentDbEventStore.cs
@@ -63,7 +63,7 @@ namespace Cqrs.Azure.DocumentDb.Events
 				using (DocumentClient client = AzureDocumentDbEventStoreConnectionStringFactory.GetEventStoreConnectionClient())
 				{
 					Database database = AzureDocumentDbHelper.CreateOrReadDatabase(client, AzureDocumentDbEventStoreConnectionStringFactory.GetEventStoreConnectionDatabaseName()).Result;
-					DocumentCollection collection = AzureDocumentDbHelper.CreateOrReadCollection(client, database, AzureDocumentDbEventStoreConnectionStringFactory.GetEventStoreConnectionCollectionName()).Result;
+					DocumentCollection collection = AzureDocumentDbHelper.CreateOrReadCollection(client, database, string.Format("{0}_{1}", AzureDocumentDbEventStoreConnectionStringFactory.GetEventStoreConnectionCollectionName(), eventData.EventType)).Result;
 
 					Logger.LogInfo("Creating document for event asyncronously", string.Format("{0}\\PersitEvent", GetType().Name));
 					client.CreateDocumentAsync(collection.SelfLink, eventData).Wait();
