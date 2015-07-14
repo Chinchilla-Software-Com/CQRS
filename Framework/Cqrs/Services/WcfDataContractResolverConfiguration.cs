695b66ef422d3ac59023e4e40757d7d9fc461ca6
grover <grover@cdmdotnet.com>
2015-07-14 13:27:00 +0000

    Added a getter function as well.
    git-svn-id: file:///data/svn/Cqrs/trunk@468 72444466-9f5c-4d9b-9c6f-db583910f96c

--- Framework/Cqrs/Services/WcfDataContractResolverConfiguration.cs
+++ Framework/Cqrs/Services/WcfDataContractResolverConfiguration.cs
@@ -7,6 +7,7 @@
 #endregion
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Cqrs.Services
@@ -15,11 +16,11 @@ namespace Cqrs.Services
 	{
 		public static WcfDataContractResolverConfiguration Current { get; protected set; }
 
-		protected IDictionary<Type, IList<Tuple<string, string>>> DataContracts { get; private set; }
+		protected IDictionary<Type, IDictionary<string, Type>> DataContracts { get; private set; }
 
 		public WcfDataContractResolverConfiguration()
 		{
-			DataContracts = new Dictionary<Type, IList<Tuple<string, string>>>();
+			DataContracts = new Dictionary<Type, IDictionary<string, Type>>();
 		}
 
 		static WcfDataContractResolverConfiguration()
@@ -27,17 +28,45 @@ namespace Cqrs.Services
 			Current = new WcfDataContractResolverConfiguration();
 		}
 
-		public void RegisterDataContract<TService, TDataContract>()
+		public void RegisterDataContract<TService, TDataContract>(string operationName)
 			where TDataContract : new ()
 		{
 			Type serviceType = typeof (TService);
-			IList<Tuple<string, string>> dataContracts;
+			IDictionary<string, Type> dataContracts;
 			if (!DataContracts.TryGetValue(serviceType, out dataContracts))
 			{
-				dataContracts = new List<Tuple<string, string>>();
-				DataContracts.Add(serviceType, dataContracts);
+				lock (DataContracts)
+				{
+					if (!DataContracts.TryGetValue(serviceType, out dataContracts))
+					{
+						dataContracts = new Dictionary<string, Type>();
+						DataContracts.Add(serviceType, dataContracts);
+					}
+				}
 			}
-			dataContracts.Add(new Tuple<string, string>(typeof(TDataContract).Assembly.FullName, typeof(TDataContract).FullName));
+			dataContracts.Add(operationName, typeof(TDataContract));
+		}
+
+		public Type GetDataContracts<TService>(string operationName)
+		{
+			Type serviceType = typeof (TService);
+			IDictionary<string, Type> dataContracts;
+			if (!DataContracts.TryGetValue(serviceType, out dataContracts))
+			{
+				lock (DataContracts)
+				{
+					if (!DataContracts.TryGetValue(serviceType, out dataContracts))
+					{
+						dataContracts = new Dictionary<string, Type>();
+						DataContracts.Add(serviceType, dataContracts);
+					}
+				}
+			}
+
+			Type dataContractType;
+			if (dataContracts.TryGetValue(operationName, out dataContractType))
+				return dataContractType;
+			return null;
 		}
 	}
 }
