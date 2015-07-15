a64b9cbe33564a3b223c5375930e7ce3962cb63e
grover <grover@cdmdotnet.com>
2015-07-15 11:32:14 +0000

    Added Cqrs.Ninject.ServiceHost project
    git-svn-id: file:///data/svn/Cqrs/trunk@475 72444466-9f5c-4d9b-9c6f-db583910f96c

---
+++ Framework/Cqrs.Ninject.ServiceHost/NinjectWcfServiceHostFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel.Description;
+using Cqrs.Services;
+using Ninject.Extensions.Wcf;
+
+namespace Cqrs.Ninject.ServiceHost
+{
+	public class NinjectWcfServiceHostFactory<TServiceType> : NinjectServiceHostFactory
+	{
+		protected override System.ServiceModel.ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses)
+		{
+			System.ServiceModel.ServiceHost host = base.CreateServiceHost(serviceType, baseAddresses);
+
+			foreach (ServiceEndpoint serviceEndpoint in host.Description.Endpoints)
+			{
+				foreach (OperationDescription operationDescription in serviceEndpoint.Contract.Operations)
+				{
+					Type dataContractType = WcfDataContractResolverConfiguration.Current.GetDataContracts<TServiceType>(operationDescription.Name);
+					if (dataContractType == null)
+						continue;
+					DataContractSerializerOperationBehavior serializerBehavior = operationDescription.Behaviors.Find<DataContractSerializerOperationBehavior>();
+					if (serializerBehavior == null)
+						operationDescription.Behaviors.Add(serializerBehavior = new DataContractSerializerOperationBehavior(operationDescription));
+					serializerBehavior.DataContractResolver = (DataContractResolver)Activator.CreateInstance(AppDomain.CurrentDomain, dataContractType.Assembly.FullName, dataContractType.FullName).Unwrap();
+				}
+			}
+
+			return host;
+		}
+	}
+}
