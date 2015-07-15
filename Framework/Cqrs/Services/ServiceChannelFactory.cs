e344f0a322613c5fb8f1e43542ee73e854de73e6
grover <grover@cdmdotnet.com>
2015-07-15 11:22:20 +0000

    Added ServiceChannelFactory.cs
    git-svn-id: file:///data/svn/Cqrs/trunk@474 72444466-9f5c-4d9b-9c6f-db583910f96c

---
+++ Framework/Cqrs/Services/ServiceChannelFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace Cqrs.Services
+{
+	public class ServiceChannelFactory<TService> : ChannelFactory<TService>
+	{
+		/// <summary>
+		/// Instanciates a new instance of the <see cref="ServiceChannelFactory{TService}"/> class with a specified endpoint configuration name.
+		/// </summary>
+		/// <param name="endpointConfigurationName">The configuration name used for the endpoint.</param>
+		public ServiceChannelFactory(string endpointConfigurationName)
+			: base(endpointConfigurationName)
+		{
+			AttachDataContractResolver(Endpoint);
+		}
+
+		protected virtual void AttachDataContractResolver(ServiceEndpoint endpoint)
+		{
+			ContractDescription contractDescription = endpoint.Contract;
+
+			foreach (OperationDescription operationDescription in contractDescription.Operations)
+			{
+				Type dataContractType = WcfDataContractResolverConfiguration.Current.GetDataContracts<TService>(operationDescription.Name);
+				if (dataContractType == null)
+					continue;
+				DataContractSerializerOperationBehavior serializerBehavior = operationDescription.Behaviors.Find<DataContractSerializerOperationBehavior>();
+				if (serializerBehavior == null)
+					operationDescription.Behaviors.Add(serializerBehavior = new DataContractSerializerOperationBehavior(operationDescription));
+				serializerBehavior.DataContractResolver = (DataContractResolver)Activator.CreateInstance(AppDomain.CurrentDomain, dataContractType.Assembly.FullName, dataContractType.FullName).Unwrap();
+			}
+		}
+
+		protected virtual void RegisterDataContracts() { }
+	}
+}
