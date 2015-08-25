38e00086e663e434d93920b0e6e2b2592168a974
grover <grover@cdmdotnet.com>
2015-08-25 15:47:06 +0000

    Corrected a missing exception message format
    git-svn-id: file:///data/svn/Cqrs/trunk@486 72444466-9f5c-4d9b-9c6f-db583910f96c

--- Framework/Cqrs/Domain/Exception/DuplicateEventException.cs
+++ Framework/Cqrs/Domain/Exception/DuplicateEventException.cs
@@ -5,7 +5,7 @@ namespace Cqrs.Domain.Exception
 	public class DuplicateEventException : System.Exception
 	{
 		public DuplicateEventException(Guid id, int version)
-			: base(string.Format("Eventstore gave more than one event for aggregate {0} for version", id, version))
+			: base(string.Format("Eventstore gave more than one event for aggregate {0} for version {1}", id, version))
 		{
 		}
 	}
