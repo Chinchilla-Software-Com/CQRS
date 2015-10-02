351ed0b05c9d0c40fd7c1a15eeaed7479f7954dc
grover <grover.jackson@results.com>
2015-10-03 00:23:43 +1300

    Missing check-ins

---
+++ Framework/Cqrs.Tests/Substitutes/TestSnapshotEventStore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cqrs.Events;
+using Cqrs.Authentication;
+
+namespace Cqrs.Tests.Substitutes
+{
+	public class TestSnapshotEventStore : TestEventStore
+	{
+		public override IEnumerable<IEvent<ISingleSignOnToken>> Get(Type aggregateType, Guid aggregateId, bool useLastEventOnly = false, int fromVersion = -1)
+		{
+			if (aggregateId == EmptyGuid || aggregateId == Guid.Empty)
+			{
+				return new List<IEvent<ISingleSignOnToken>>();
+			}
+
+			return new List<IEvent<ISingleSignOnToken>>
+			{
+				new TestAggregateDidSomething {Id = aggregateId, Version = 1},
+				new TestAggregateDidSomeethingElse {Id = aggregateId, Version = 2},
+				new TestAggregateDidSomething {Id = aggregateId, Version = 3}
+			}
+			.Where(x => x.Version > fromVersion);
+		}
+	}
+}
