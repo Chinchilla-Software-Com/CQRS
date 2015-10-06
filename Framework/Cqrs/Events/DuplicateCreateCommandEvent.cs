69c6ff957cede066ce531a00183d19b387fcb57d
grover <grover.jackson@results.com>
2015-10-06 23:20:29 +1300

    Added DuplicateCreateCommandEvent

---
+++ Framework/Cqrs/Events/DuplicateCreateCommandEvent.cs
@@ -0,0 +1,46 @@
+﻿#region Copyright
+// // -----------------------------------------------------------------------
+// // <copyright company="cdmdotnet Limited">
+// // 	Copyright cdmdotnet Limited. All rights reserved.
+// // </copyright>
+// // -----------------------------------------------------------------------
+#endregion
+
+using System;
+using Cqrs.Messages;
+
+namespace Cqrs.Events
+{
+	public class DuplicateCreateCommandEvent<TAuthenticationToken> : IEvent<TAuthenticationToken>
+	{
+		#region Implementation of IMessage
+
+		public Guid CorrolationId { get; set; }
+
+		public Guid CorrelationId { get; set; }
+
+		public FrameworkType Framework { get; set; }
+
+		#endregion
+
+		#region Implementation of IMessageWithAuthenticationToken<TAuthenticationToken>
+
+		public TAuthenticationToken AuthenticationToken { get; set; }
+
+		#endregion
+
+		#region Implementation of IEvent<TAuthenticationToken>
+
+		public Guid Id { get; set; }
+
+		public int Version { get; set; }
+
+		public DateTimeOffset TimeStamp { get; set; }
+
+		#endregion
+
+		public Type AggregateType { get; set; }
+
+		public Guid AggregateRsn { get; set; }
+	}
+}
