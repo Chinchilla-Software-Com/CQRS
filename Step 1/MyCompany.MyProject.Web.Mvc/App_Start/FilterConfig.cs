3ca40b27093210748f4311f1ece07b2ef533c67e
grover <grover.jackson@results.com>
2016-02-23 23:03:13 +1300

    Raw, blank project

---
+++ Step 1/MyCompany.MyProject.Web.Mvc/App_Start/FilterConfig.cs
@@ -0,0 +1,20 @@
+﻿#region Copyright
+// // -----------------------------------------------------------------------
+// // <copyright company="cdmdotnet Limited">
+// // 	Copyright cdmdotnet Limited. All rights reserved.
+// // </copyright>
+// // -----------------------------------------------------------------------
+#endregion
+
+using System.Web.Mvc;
+
+namespace MyCompany.MyProject.Web.Mvc
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
