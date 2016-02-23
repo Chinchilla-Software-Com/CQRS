36d0150535a54ba08a1570a58964c3ad673c7aba
grover <grover.jackson@results.com>
2016-02-24 00:06:39 +1300

    Added Step 2 content

---
+++ Step 2/MyCompany.MyProject.Web.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+using Cqrs.Authentication;
+using cdmdotnet.Logging;
+using Cqrs.Services;
+
+namespace MyCompany.MyProject.Web.Mvc.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
