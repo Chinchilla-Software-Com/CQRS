d732a6034ace80945d83e8b158f7b2b8770d4006
grover <grover.jackson@results.com>
2016-02-24 00:48:52 +1300

    Added missing files

---
+++ Step 2/MyCompany.MyProject.Domain.Ninject/MyCompany/MyProject/Configuration/BaseModule.cs
@@ -0,0 +1,30 @@
+﻿using cdmdotnet.Logging;
+using cdmdotnet.Logging.Configuration;
+using Ninject.Modules;
+
+namespace MyCompany.MyProject.Configuration
+{
+	public class BaseModule : NinjectModule
+	{
+		public override void Load()
+		{
+			Bind<ILoggerSettings>()
+				.To<LoggerSettingsConfigurationSection>()
+				.InSingletonScope();
+
+			RegisterLogger();
+		}
+
+		/// <summary>
+		/// Register the <see cref="ILogger"/>
+		/// </summary>
+		protected virtual void RegisterLogger()
+		{
+			Kernel.Unbind<ILogger>();
+
+			Bind<ILogger>()
+				.To<ConsoleLogger>()
+				.InSingletonScope();
+		}
+	}
+}
