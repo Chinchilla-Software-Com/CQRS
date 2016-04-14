0343571cdffecf74631a76f3512ea71673849954
grover <grover.jackson@results.com>
2016-04-14 18:10:24 +1200

    Re-added step 4

---
+++ NorthwindDemo/step-4/aspnet-mvc/Northwind.Domain/Northwind/Domain/Orders/Repositories/Queries/Strategies/InvoicesQueryStrategyBuilder.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cqrs.Repositories.Queries;
+
+namespace Northwind.Domain.Orders.Repositories.Queries.Strategies
+{
+	public partial class InvoicesQueryStrategyBuilder
+	{
+
+		partial void GeneratePredicateWithRsn(SortedSet<QueryParameter> parameters, IQueryable<Entities.InvoicesEntity> leftHandQueryable, ref IQueryable<Entities.InvoicesEntity> resultingQueryable)
+		{
+			var rsn = parameters.GetValue<Guid>(0);
+
+			var query = (resultingQueryable ?? leftHandQueryable ?? GetEmptyQueryPredicate());
+
+			resultingQueryable = query.Where
+			(
+				invoice => invoice.Rsn == rsn
+			);
+		}
+	}
+}
