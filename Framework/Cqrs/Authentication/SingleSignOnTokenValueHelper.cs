261d5b72dc234a9d70144e6a15a37ddfd57492d4
Grover Jackson <grover@cdmdotnet.com>
2016-05-19 11:57:30 +1200

    Added a a web based token value helper

--- Framework/Cqrs/Authentication/SingleSignOnTokenValueHelper.cs
+++ Framework/Cqrs/Authentication/SingleSignOnTokenValueHelper.cs
@@ -12,18 +12,18 @@ namespace Cqrs.Authentication
 {
 	public class SingleSignOnTokenValueHelper : IAuthenticationTokenHelper<ISingleSignOnToken>
 	{
-		private const string CallContextPermissoinScopeValueKey = "SingleSignOnTokenValue";
+		private const string CallContextPermissionScopeValueKey = "SingleSignOnTokenValue";
 
 		#region Implementation of IAuthenticationTokenHelper<out ISingleSignOnToken>
 
 		public ISingleSignOnToken GetAuthenticationToken()
 		{
-			return (ISingleSignOnToken)CallContext.GetData(CallContextPermissoinScopeValueKey);
+			return (ISingleSignOnToken)CallContext.GetData(CallContextPermissionScopeValueKey);
 		}
 
 		public ISingleSignOnToken SetAuthenticationToken(ISingleSignOnToken value)
 		{
-			CallContext.SetData(CallContextPermissoinScopeValueKey, value);
+			CallContext.SetData(CallContextPermissionScopeValueKey, value);
 			return value;
 		}
 
---
+++ Framework/Cqrs/Authentication/WebSingleSignOnTokenValueHelper.cs
@@ -0,0 +1,32 @@
+﻿#region Copyright
+// // -----------------------------------------------------------------------
+// // <copyright company="cdmdotnet Limited">
+// // 	Copyright cdmdotnet Limited. All rights reserved.
+// // </copyright>
+// // -----------------------------------------------------------------------
+#endregion
+
+using System.Web;
+
+namespace Cqrs.Authentication
+{
+	public class WebSingleSignOnTokenValueHelper : IAuthenticationTokenHelper<ISingleSignOnToken>
+	{
+		private const string CallContextPermissionScopeValueKey = "SingleSignOnTokenValue";
+
+		#region Implementation of IAuthenticationTokenHelper<out ISingleSignOnToken>
+
+		public ISingleSignOnToken GetAuthenticationToken()
+		{
+			return (ISingleSignOnToken)HttpContext.Current.Items[CallContextPermissionScopeValueKey];
+		}
+
+		public ISingleSignOnToken SetAuthenticationToken(ISingleSignOnToken value)
+		{
+			HttpContext.Current.Items[CallContextPermissionScopeValueKey] = value;
+			return value;
+		}
+
+		#endregion
+	}
+}
