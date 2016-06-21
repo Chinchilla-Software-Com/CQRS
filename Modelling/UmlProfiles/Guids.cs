088bbf6e547ef5f050b1a4c7e5d43023a41b9a08
Grover Jackson <grover@cdmdotnet.com>
2016-06-21 21:35:23 +1200

    Added solution template Added ability to store solution configuration settings within the solution file itself for later use by the extension.

--- Modelling/UmlProfiles/Guids.cs
+++ Modelling/UmlProfiles/Guids.cs
@@ -4,11 +4,18 @@ using System;
 
 namespace Cqrs.Modelling.UmlProfiles
 {
-    static class GuidList
-    {
-        public const string guidvsp1PkgString = "36bac4cc-8397-4d27-aa64-2a01a2c4d059";
-        public const string guidvsp1CmdSetString = "defe0f21-3b6a-48fa-a673-0fb40316fe45";
+	static class GuidList
+	{
+		public const string guidvsp1PkgString = "36bac4cc-8397-4d27-aa64-2a01a2c4d059";
+		public const string guidvsp1CmdSetString = "defe0f21-3b6a-48fa-a673-0fb40316fe45";
 
-        public static readonly Guid guidvsp1CmdSet = new Guid(guidvsp1CmdSetString);
-    };
+		public static readonly Guid guidvsp1CmdSet = new Guid(guidvsp1CmdSetString);
+	};
+
+	static class PkgCmdIDList
+	{
+		public const uint cmdidMyCommand = 0x200;
+		public const uint cmdidMyTool = 0x201;
+
+	};
 }
--- Modelling/UmlProfiles/UmlPackage.cs
+++ Modelling/UmlProfiles/UmlPackage.cs
@@ -1,21 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Script.Serialization;
 using Cqrs.Modelling.UmlProfiles.Code;
-using Encryption;
 using EnvDTE;
 using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.Win32;
 using VSLangProj;
 
 namespace Cqrs.Modelling.UmlProfiles
@@ -43,7 +44,8 @@ namespace Cqrs.Modelling.UmlProfiles
 	// And make it tabbed to the "Solution Explorer" Window
 	[ProvideToolWindow(typeof(CqrsSolutionOptionsToolWindow), Style = VsDockStyle.Tabbed, Window = "3ae79031-e1bc-11d0-8f78-00a0c9110057")]
 	[ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExistsAndFullyLoaded_string)]
-	public sealed class UmlPackage : Package
+	[ProvideSolutionProperties(CqrsNetSettingsKey)]
+	public sealed class UmlPackage : Package, IVsPersistSolutionProps
 	{
 		/// <summary>
 		/// Default constructor of the package.
@@ -60,6 +62,12 @@ namespace Cqrs.Modelling.UmlProfiles
 
 		internal static IList<Project> ProjectNames { get; private set; }
 
+		internal static IVsPersistSolutionProps Solution { get; private set; }
+
+		internal static bool IsDirty { get; set; }
+
+		internal static CqrsSettings CqrsSettings { get; set; }
+
 		/////////////////////////////////////////////////////////////////////////////
 		// Overriden Package Implementation
 		#region Package Members
@@ -75,53 +83,19 @@ namespace Cqrs.Modelling.UmlProfiles
 
 			try
 			{
+				ProjectNames = new List<Project>();
+
 				DTE dte = (DTE)GetService(typeof(DTE));
 
-				Projects projects = dte.Solution.Projects;
 				dte.Events.SolutionEvents.ProjectAdded += project => { ProjectNames.Add(project); };
 				dte.Events.SolutionEvents.ProjectRemoved += project => { if (ProjectNames.Contains(project)) { ProjectNames.Remove(project); } };
 				dte.Events.SolutionEvents.ProjectRenamed += (project, originalName) => { };
 
-				ProjectNames = new List<Project>();
+				dte.Events.SolutionEvents.Opened += () => { PopulateProjectNames(dte); };
 
-				string solutionName = dte.Solution.FullName;
-				solutionName = solutionName.Substring(solutionName.LastIndexOf('\\') + 1);
-				foreach (Project project in projects)
-				{
-					ProjectNames.Add(project);
-					IEnumerable<AssemblyName> references;
-					try
-					{
-						references = CollectSettings(project)
-							.Where(assembly => assembly.Name.StartsWith("Cqrs"))
-							.ToList();
-					}
-					catch (NullReferenceException)
-					{
-						continue;
-					}
-					foreach (AssemblyName assemblyName in references)
-					{
-						string urlPath = string.Format("/{0}/{1}/{2}/{3}/{4}/{5}/{6}/{7}", HttpUtility.UrlPathEncode(dte.Version), HttpUtility.UrlPathEncode(dte.Edition), HttpUtility.UrlPathEncode(solutionName), HttpUtility.UrlPathEncode(project.Name), HttpUtility.UrlPathEncode(project.Kind), HttpUtility.UrlPathEncode(project.UniqueName.Replace('\\', '¿')), HttpUtility.UrlPathEncode(assemblyName.Name), HttpUtility.UrlPathEncode(assemblyName.Version.ToString()));
+				dte.Events.SolutionEvents.BeforeClosing += ProjectNames.Clear;
 
-						HttpWebRequest request = WebRequest.Create(string.Format("https://www.chinchillasoftware.com/VisualStudio{0}", urlPath)) as HttpWebRequest;
-						request.Method = "GET";
-						try
-						{
-							new TaskFactory().StartNew(() =>
-							{
-								using (request.GetResponse())
-								{
-									Debug.WriteLine("Called '{0}'.", request.Address);
-								}
-							});
-							Debug.WriteLine("Calling '{0}'.", request.Address);
-						}
-						catch
-						{
-						}
-					}
-				}
+				PopulateProjectNames(dte);
 			}
 			catch(Exception ex)
 			{
@@ -139,7 +113,82 @@ namespace Cqrs.Modelling.UmlProfiles
 			{
 				Trace.TraceError(ex.Message);
 			}
+
+			try
+			{
+
+				// Add our command handlers for menu (commands must exist in the .vsct file)
+				OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+				if (null != mcs)
+				{
+					// Create the command for the menu item.
+					CommandID menuCommandID = new CommandID(GuidList.guidvsp1CmdSet, (int)PkgCmdIDList.cmdidMyCommand);
+					MenuCommand menuItem = new MenuCommand(MenuItemCallback, menuCommandID);
+					mcs.AddCommand(menuItem);
+					// Create the command for the tool window
+					CommandID toolwndCommandID = new CommandID(GuidList.guidvsp1CmdSet, (int)PkgCmdIDList.cmdidMyTool);
+					MenuCommand menuToolWin = new MenuCommand(ShowToolWindow, toolwndCommandID);
+					mcs.AddCommand(menuToolWin);
+				}
+			}
+			catch (Exception ex)
+			{
+				Trace.TraceError(ex.Message);
+			}
 		}
+
+		private static void PopulateProjectNames(DTE dte)
+		{
+			Solution = (IVsPersistSolutionProps)GetGlobalService(typeof(IVsPersistSolutionProps));
+
+			Projects projects = dte.Solution.Projects;
+
+			string solutionName = dte.Solution.FullName;
+			solutionName = solutionName.Substring(solutionName.LastIndexOf('\\') + 1);
+
+			foreach (Project project in projects)
+			{
+				ProjectNames.Add(project);
+				IEnumerable<AssemblyName> references;
+				try
+				{
+					references = CollectSettings(project)
+						.Where(assembly => assembly.Name.StartsWith("Cqrs"))
+						.ToList();
+				}
+				catch (NullReferenceException)
+				{
+					continue;
+				}
+				foreach (AssemblyName assemblyName in references)
+				{
+					string urlPath = string.Format("/{0}/{1}/{2}/{3}/{4}/{5}/{6}/{7}", HttpUtility.UrlPathEncode(dte.Version),
+						HttpUtility.UrlPathEncode(dte.Edition), HttpUtility.UrlPathEncode(solutionName),
+						HttpUtility.UrlPathEncode(project.Name), HttpUtility.UrlPathEncode(project.Kind),
+						HttpUtility.UrlPathEncode(project.UniqueName.Replace('\\', '¿')), HttpUtility.UrlPathEncode(assemblyName.Name),
+						HttpUtility.UrlPathEncode(assemblyName.Version.ToString()));
+
+					HttpWebRequest request =
+						WebRequest.Create(string.Format("https://www.chinchillasoftware.com/VisualStudio{0}", urlPath)) as HttpWebRequest;
+					request.Method = "GET";
+					try
+					{
+						new TaskFactory().StartNew(() =>
+						{
+							using (request.GetResponse())
+							{
+								Debug.WriteLine("Called '{0}'.", request.Address);
+							}
+						});
+						Debug.WriteLine("Calling '{0}'.", request.Address);
+					}
+					catch
+					{
+					}
+				}
+			}
+		}
+
 		#endregion
 
 		public static IEnumerable<AssemblyName> GetAssemblyReferences(Project project)
@@ -232,6 +281,279 @@ namespace Cqrs.Modelling.UmlProfiles
 			IVsWindowFrame windowFrame = (IVsWindowFrame)window.Frame;
 			Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(windowFrame.Show());
 		}
+
+		/// <summary>
+		/// This function is the callback used to execute a command when the a menu item is clicked.
+		/// See the Initialize method to see how the menu item is associated to this function using
+		/// the OleMenuCommandService service and the MenuCommand class.
+		/// </summary>
+		private void MenuItemCallback(object sender, EventArgs e)
+		{
+			// Show a Message Box to prove we were here
+			IVsUIShell uiShell = (IVsUIShell)GetService(typeof(SVsUIShell));
+			Guid clsid = Guid.Empty;
+			int result;
+			Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(uiShell.ShowMessageBox(
+					   0,
+					   ref clsid,
+					   "VSPackage",
+					   string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.ToString()),
+					   string.Empty,
+					   0,
+					   OLEMSGBUTTON.OLEMSGBUTTON_OK,
+					   OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST,
+					   OLEMSGICON.OLEMSGICON_INFO,
+					   0,        // false
+					   out result));
+		}
+
+		private const string CqrsNetSettingsKey = "CqrsNetSettings";
+
+		public int SaveUserOptions(IVsSolutionPersistence pPersistence)
+		{
+			try
+			{
+				pPersistence.SavePackageUserOpts(this, CqrsNetSettingsKey);
+
+				return VSConstants.S_OK;
+			}
+			finally
+			{
+				Marshal.ReleaseComObject(pPersistence); // See Package.cs from MPF for reason
+			}
+		}
+
+		public int LoadUserOptions(IVsSolutionPersistence pPersistence, uint grfLoadOpts)
+		{
+			try
+			{
+				pPersistence.LoadPackageUserOpts(this, CqrsNetSettingsKey);
+			}
+			finally
+			{
+				Marshal.ReleaseComObject(pPersistence);
+			}
+
+			return VSConstants.S_OK;
+		}
+
+		public int WriteUserOptions(IStream pOptionsStream, string pszKey)
+		{
+			try
+			{
+				using (ComStreamWrapper wrapper = new ComStreamWrapper(pOptionsStream))
+				{
+					switch (pszKey)
+					{
+						case CqrsNetSettingsKey:
+							SerializeEnlistData(wrapper, true);
+							break;
+					}
+				}
+
+				return VSConstants.S_OK;
+			}
+			finally
+			{
+				Marshal.ReleaseComObject(pOptionsStream); // See Package.cs from MPF for reason
+			}
+		}
+
+		public int ReadUserOptions(IStream pOptionsStream, string pszKey)
+		{
+			try
+			{
+				using (ComStreamWrapper wrapper = new ComStreamWrapper(pOptionsStream, true))
+				{
+					switch (pszKey)
+					{
+						case CqrsNetSettingsKey:
+							SerializeEnlistData(wrapper, false);
+							break;
+					}
+				}
+				return VSConstants.S_OK;
+			}
+			finally
+			{
+				Marshal.ReleaseComObject(pOptionsStream);
+			}
+		}
+
+		public int QuerySaveSolutionProps(IVsHierarchy pHierarchy, VSQUERYSAVESLNPROPS[] pqsspSave)
+		{
+			// This function is called by the IDE to determine if something needs to be saved in the solution.
+			// If the package returns that it has dirty properties, the shell will callback on SaveSolutionProps
+
+			if (pHierarchy == null)
+			{
+				VSQUERYSAVESLNPROPS result = VSQUERYSAVESLNPROPS.QSP_HasNoProps;
+
+				if (CqrsSettings != null)
+				{
+					if (IsDirty)
+						result = VSQUERYSAVESLNPROPS.QSP_HasDirtyProps;
+					else
+						result = VSQUERYSAVESLNPROPS.QSP_HasNoDirtyProps;
+				}
+				pqsspSave[0] = result;
+			}
+
+			return VSConstants.S_OK;
+		}
+
+		public int SaveSolutionProps(IVsHierarchy pHierarchy, IVsSolutionPersistence pPersistence)
+		{
+			// This function gets called by the shell after QuerySaveSolutionProps returned QSP_HasDirtyProps
+
+			// The package will pass in the key under which it wants to save its properties, 
+			// and the IDE will call back on WriteSolutionProps
+
+			// The properties will be saved in the Pre-Load section
+			// When the solution will be reopened, the IDE will call our package to load them back before the projects in the solution are actually open
+			// This could help if the source control package needs to persist information like projects translation tables, that should be read from the suo file
+			// and should be available by the time projects are opened and the shell start calling IVsSccEnlistmentPathTranslation functions.
+			if (pHierarchy == null) // Only save the property on the solution itself
+			{
+				// SavePackageSolutionProps will call WriteSolutionProps with the specified key
+
+				if (CqrsSettings != null)
+					pPersistence.SavePackageSolutionProps(1 /* TRUE */, null, this, CqrsNetSettingsKey);
+
+				// Once we saved our props, the solution is not dirty anymore
+				IsDirty = false;
+			}
+
+			return VSConstants.S_OK;
+		}
+
+		public int WriteSolutionProps(IVsHierarchy pHierarchy, string pszKey, IPropertyBag pPropBag)
+		{
+			if (pHierarchy != null)
+				return VSConstants.S_OK; // Not sent by our code!
+
+			if (pPropBag == null)
+				return VSConstants.E_POINTER;
+
+			// This method is called from the VS implementation after a request from SaveSolutionProps
+
+			using (IPropertyMap map = GetMap(pPropBag))
+			{
+				switch (pszKey)
+				{
+					case CqrsNetSettingsKey:
+						map.SetRawValue(CqrsNetSettingsKey, new JavaScriptSerializer().Serialize(CqrsSettings));
+						map.Flush();
+						break;
+				}
+			}
+
+			return VSConstants.S_OK;
+		}
+
+		public int ReadSolutionProps(IVsHierarchy pHierarchy, string pszProjectName, string pszProjectMk, string pszKey, int fPreLoad, IPropertyBag pPropBag)
+		{
+			if (pHierarchy != null)
+				return VSConstants.S_OK;
+
+			using (IPropertyMap map = GetMap(pPropBag))
+			{
+				bool preload = (fPreLoad != 0);
+
+				switch (pszKey)
+				{
+					case CqrsNetSettingsKey:
+						if (preload)
+						{
+							string value;
+							bool register = map.TryGetValue(CqrsNetSettingsKey, out value);
+
+							if (!register || string.IsNullOrEmpty(value))
+								register = false;
+
+							if (register)
+								CqrsSettings = new JavaScriptSerializer().Deserialize<CqrsSettings>(value);
+							
+						}
+						break;
+				}
+			}
+
+			return VSConstants.S_OK;
+		}
+
+		public int OnProjectLoadFailure(IVsHierarchy pStubHierarchy, string pszProjectName, string pszProjectMk, string pszKey)
+		{
+			return VSConstants.S_OK;
+		}
+
+		public IPropertyMap GetMap(IPropertyBag propertyBag)
+		{
+			return new PropertyBag(propertyBag);
+		}
+
+		/// <summary>
+		/// Serializes the enlist data.
+		/// </summary>
+		/// <param name="store">The store.</param>
+		/// <param name="writeData">if set to <c>true</c> [write data].</param>
+		private void SerializeEnlistData(Stream store, bool writeData)
+		{
+			if (writeData)
+				WriteEnlistData(store);
+			else
+				ReadEnlistUserData(store);
+		}
+
+		const int EnlistSerializerVersion = 1;
+		EnlistBase enlist = null;
+
+		private void ReadEnlistUserData(Stream store)
+		{
+			if (store.Length < 2 * sizeof(int))
+				return;
+
+			using (BinaryReader br = new BinaryReader(store))
+			{
+				int version = br.ReadInt32(); // The enlist version used to write the data
+				int requiredVersion = br.ReadInt32(); // All enlist versions older then this should ignore all data
+
+				if ((requiredVersion > EnlistSerializerVersion) || version < requiredVersion)
+					return; // Older versions (we) should ignore this data
+
+				int count = br.ReadInt32(); // 1
+
+				int stringCount = br.ReadInt32();
+
+				List<string> values = new List<string>();
+
+				for (int j = 0; j < stringCount; j++)
+					values.Add(br.ReadString());
+
+				enlist = new EnlistBase();
+				enlist.LoadUserData(values);
+			}
+		}
+
+		private void WriteEnlistData(Stream store)
+		{
+			if (enlist == null)
+				return;
+
+			using (BinaryWriter bw = new BinaryWriter(store))
+			{
+				bw.Write((int)1); // Minimum version required to read these settings; update if incompatible
+				bw.Write((int)EnlistSerializerVersion); // Writer version
+
+				bw.Write(1);
+				string[] values = enlist.GetUserData();
+
+				bw.Write(values.Length);
+
+				foreach (string s in values)
+					bw.Write(s);
+			}
+		}
 	}
 
 	static class Extensions
@@ -239,6 +561,35 @@ namespace Cqrs.Modelling.UmlProfiles
 		public static string Or(this string text, string alternative)
 		{
 			return string.IsNullOrWhiteSpace(text) ? alternative : text;
+		}
+	}
+
+	internal class CqrsSettings
+	{
+		public string ModellingProjectName { get; set; }
+
+		public string PublicEventProjectName { get; set; }
+
+		public string PrivateEventProjectName { get; set; }
+	}
+
+	class EnlistBase
+	{
+		string[] _userData;
+
+		public virtual void LoadUserData(List<string> values)
+		{
+			_userData = values.ToArray();
+		}
+
+		internal string[] GetUserData()
+		{
+			return (string[])(_userData ?? new string[0]).Clone();
+		}
+
+		internal bool ShouldSerialize()
+		{
+			return true;
 		}
 	}
 }
