1c565bc76cc478aad1089a140abd415cfa7db08b
Grover Jackson <grover@cdmdotnet.com>
2016-10-29 19:26:03 +1300

    Added the EventStoreRetentionLevel Attribute to allow custom event publishers to use a standardised metric to decide hot, warm and cold storage of events.

---
+++ Framework/Cqrs/Events/EventStoreRetentionLevelAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cqrs.Events
+{
+	/// <summary>
+	/// Provides a mechanism to configure different <see cref="IEventStore{TAuthenticationToken}"/> instances to save the event to.
+	/// This is useful if you have events that should be in hot storage with quick loads and events that should be in cold storage and are unlikely to be reloaded and use slower cheaper storage.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class)]
+	public class EventStoreRetentionLevelAttribute : Attribute
+	{
+		/// <summary>
+		/// The level of retention required. By specifying a value we look for a matching configured <see cref="IEventStore{TAuthenticationToken}"/> with the same level defined.
+		/// </summary>
+		public short RetentionLevel { get; set; }
+	}
+}
