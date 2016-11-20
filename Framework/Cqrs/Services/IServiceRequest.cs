657eb34c3ada98335480bcd85d44e79982782716
Grover Jackson <grover@cdmdotnet.com>
2016-11-20 16:59:05 +1300

    Added CqrsEventApiController

--- Framework/Cqrs/Services/IServiceRequest.cs
+++ Framework/Cqrs/Services/IServiceRequest.cs
@@ -3,11 +3,21 @@ using System.Runtime.Serialization;
 
 namespace Cqrs.Services
 {
+	/// <summary>
+	/// A request message envelope that holds authentication and correlation information used when making public API requests.
+	/// </summary>
+	/// <typeparam name="TAuthenticationToken">The <see cref="Type">type</see> of authentication token used.</typeparam>
 	public interface IServiceRequest<TAuthenticationToken>
 	{
+		/// <summary>
+		/// The authentication token used to identify the requester.
+		/// </summary>
 		[DataMember]
 		TAuthenticationToken AuthenticationToken { get; set; }
 
+		/// <summary>
+		/// The correlation id used to group together events and notifications.
+		/// </summary>
 		[DataMember]
 		Guid CorrelationId { get; set; }
 	}
