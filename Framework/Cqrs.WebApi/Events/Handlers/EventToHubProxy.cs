aad6f3c30239ecf7e5cc57bfab43460a3f62fd20
Grover Jackson <grover@cdmdotnet.com>
2016-12-01 16:38:31 +1300

    * Added a WebAPI controller * Added a SignalR Hub and the class to proxy events from the message bus out over singalR.

---
+++ Framework/Cqrs.WebApi/Events/Handlers/EventToHubProxy.cs
@@ -0,0 +1,24 @@
+﻿using Cqrs.Authentication;
+using Cqrs.Events;
+
+namespace Cqrs.WebApi.Events.Handlers
+{
+	public abstract class EventToHubProxy<TSingleSignOnToken>
+		where TSingleSignOnToken : ISingleSignOnToken, new()
+	{
+		protected EventToHubProxy(NotificationHub notificationHub, IAuthenticationTokenHelper<TSingleSignOnToken> authenticationTokenHelper)
+		{
+			NotificationHub = notificationHub;
+			AuthenticationTokenHelper = authenticationTokenHelper;
+		}
+
+		protected NotificationHub NotificationHub { get; private set; }
+
+		protected IAuthenticationTokenHelper<TSingleSignOnToken> AuthenticationTokenHelper { get; private set; }
+
+		protected virtual void HandleGenericEvent(IEvent<TSingleSignOnToken> message)
+		{
+			NotificationHub.SendUserEvent(message, (AuthenticationTokenHelper.GetAuthenticationToken().Token ?? string.Empty).Replace(".", string.Empty));
+		}
+	}
+}
---
+++ Framework/Cqrs.WebApi/SignalR/Hubs/SignalRStartUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Newtonsoft.Json;
+using Owin;
+
+[assembly: OwinStartup(typeof(Cqrs.WebApi.SignalR.Hubs.SignalRStartUp))]
+namespace Cqrs.WebApi.SignalR.Hubs
+{
+	public class SignalRStartUp
+	{
+		public virtual void Configuration(IAppBuilder app)
+		{
+			// Any connection or hub wire up and configuration should go here
+			app.MapSignalR();
+
+			JsonSerializer serializer = JsonSerializer.Create(GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings);
+			GlobalHost.DependencyResolver.Register(typeof(JsonSerializer), () => serializer);
+		}
+	}
+}
