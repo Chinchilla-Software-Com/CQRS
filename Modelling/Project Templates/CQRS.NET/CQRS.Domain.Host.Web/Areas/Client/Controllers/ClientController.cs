4ea045d8aa7d351c959f46ae5f3e61e0af732c5a
Grover Jackson <grover@cdmdotnet.com>
2017-02-18 23:42:17 +1300

    Updated T4 templates to use the Blob storage repository Updated the Web API to have better handling for auto-generated crud methods.

--- Modelling/Project Templates/CQRS.NET/CQRS.Domain.Host.Web/Areas/Client/Controllers/ClientController.cs
+++ Modelling/Project Templates/CQRS.NET/CQRS.Domain.Host.Web/Areas/Client/Controllers/ClientController.cs
@@ -1,17 +1,70 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Description;
 using System.Web.Mvc;
 
-namespace $safeprojectname$.Areas.Client.Controllers
+namespace Results.Domain.Host.Web.Areas.Client.Controllers
 {
 	public class ClientController : Controller
 	{
 		public virtual PartialViewResult Index()
 		{
-			var apiExplorer = GlobalConfiguration.Configuration.Services.GetApiExplorer();
-			var apiMethods = apiExplorer.ApiDescriptions.Select(ad => new ApiMethodModel(ad)).ToList();
+			IApiExplorer apiExplorer = GlobalConfiguration.Configuration.Services.GetApiExplorer();
+			IDictionary<string, IList<ApiDescription>> apiDescriptions = new Dictionary<string, IList<ApiDescription>>();
+			foreach (ApiDescription apiDescription in apiExplorer.ApiDescriptions)
+			{
+				IList<ApiDescription> list;
+				if (!apiDescriptions.TryGetValue(apiDescription.ActionDescriptor.ActionName, out list))
+				{
+					list = new List<ApiDescription>();
+					apiDescriptions.Add(apiDescription.ActionDescriptor.ActionName, list);
+				}
+				list.Add(apiDescription);
+			}
+			IList<ApiMethodModel> apiMethods = new List<ApiMethodModel>();
+			foreach (KeyValuePair<string, IList<ApiDescription>> apiDescription in apiDescriptions)
+			{
+				IList<ApiDescription> ads = apiDescription.Value;
+				if (ads.Count == 1)
+					apiMethods.Add(new ApiMethodModel(ads.Single()));
+				if (ads.Count == 2)
+				{
+					string actionName0 = ads[0].ActionDescriptor.ActionName;
+					string actionName1 = ads[1].ActionDescriptor.ActionName;
+					if (actionName0 == actionName1)
+					{
+						var action0 = new ApiMethodModel(ads[0]);
+						ApiMethodModel action1;
+						if (!ads[0].RelativePath.Contains("{"))
+						{
+							action1 = action0;
+							action0 = new ApiMethodModel(ads[1]);
+						}
+						else
+							action1 = new ApiMethodModel(ads[1]);
+						if (action0.ActionName.StartsWith("Delete"))
+							action0.ActionName = "Delete";
+						else if (action0.ActionName.StartsWith("Update"))
+							action0.ActionName = "Update";
+						else if (action0.ActionName.StartsWith("Create"))
+							action0.ActionName = "Create";
+						apiMethods.Add(action0);
+						apiMethods.Add(action1);
+					}
+				}
+				else
+				{
+					for (int i = 0; i < ads.Count; i++)
+					{
+						ApiDescription description = ads[i];
+						var action = new ApiMethodModel(description);
+						action.ActionName = action.ActionName + i;
+						apiMethods.Add(action);
+					}
+				}
+			}
 			HttpContext.Response.ContentType = "application/javascript";
 			return PartialView(apiMethods);
 		}
