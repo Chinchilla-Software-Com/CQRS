0866a6a9659e1804dc331df7d8b9d9a3ab82a6d0
Grover Jackson <grover@cdmdotnet.com>
2017-03-18 22:31:46 +1300

    Missing file

---
+++ Framework/Cqrs/Commands/ICommandPublisher.cs
@@ -0,0 +1,24 @@
+﻿#region Copyright
+// // -----------------------------------------------------------------------
+// // <copyright company="cdmdotnet Limited">
+// // 	Copyright cdmdotnet Limited. All rights reserved.
+// // </copyright>
+// // -----------------------------------------------------------------------
+#endregion
+
+using System.Collections.Generic;
+
+namespace Cqrs.Commands
+{
+	/// <summary>
+	/// Publishes an <see cref="ICommand{TAuthenticationToken}"/>
+	/// </summary>
+	public interface ICommandPublisher<TAuthenticationToken>
+	{
+		void Publish<TCommand>(TCommand command)
+			where TCommand : ICommand<TAuthenticationToken>;
+
+		void Publish<TCommand>(IEnumerable<TCommand> commands)
+			where TCommand : ICommand<TAuthenticationToken>;
+	}
+}
