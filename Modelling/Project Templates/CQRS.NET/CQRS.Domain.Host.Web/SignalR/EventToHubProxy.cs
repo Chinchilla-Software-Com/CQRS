9fc7f65dc5c586828a4541b163ab7f74aba74b37
Grover Jackson <grover@cdmdotnet.com>
2017-03-19 00:28:31 +1300

    Corrected the referenced logging library. Corrected several errors in the template VISX.

--- Modelling/Project Templates/CQRS.NET/CQRS.Domain.Host.Web/SignalR/EventToHubProxy.cs
+++ Modelling/Project Templates/CQRS.NET/CQRS.Domain.Host.Web/SignalR/EventToHubProxy.cs
@@ -1,10 +1,11 @@
-﻿using Cqrs.Authentication;
+﻿using cdmdotnet.Logging;
+using Cqrs.Authentication;
 
 namespace $safeprojectname$.SignalR
 {
 	public partial class EventToHubProxy : Cqrs.WebApi.Events.Handlers.EventToHubProxy<SingleSignOnToken>
 	{
-		public EventToHubProxy(cdmdotnet.Logging.ILogger logger, Cqrs.WebApi.SignalR.Hubs.NotificationHub notificationHub, IAuthenticationTokenHelper<SingleSignOnToken> authenticationTokenHelper)
+		public EventToHubProxy(ILogger logger, Cqrs.WebApi.SignalR.Hubs.NotificationHub notificationHub, IAuthenticationTokenHelper<SingleSignOnToken> authenticationTokenHelper)
 			: base(logger, notificationHub, authenticationTokenHelper)
 		{
 		}
