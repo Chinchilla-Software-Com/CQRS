4f4505ebb405c2447f50e3d96423c7d194d416ea
XsamX <xsamx@users.noreply.github.com>
2017-07-25 22:56:42 +1200

    Updated coverage generation settings.

--- wiki/docs/generate-coverage-report.sh
+++ wiki/docs/generate-coverage-report.sh
@@ -1,3 +1,3 @@
-python -m coverxygen --xml-dir ./$1/xml/ --output doc-coverage.info --src-dir ../../Framework/ --scope public,protected
+python -m coverxygen --xml-dir ./$1/xml/ --output doc-coverage.info --src-dir ../../Framework/ --scope public,protected --kind enum,varible,function,class,struct,define,file,page
 lcov --summary doc-coverage.info
 genhtml --no-function-coverage --no-branch-coverage doc-coverage.info -o $1/coverage-report/
