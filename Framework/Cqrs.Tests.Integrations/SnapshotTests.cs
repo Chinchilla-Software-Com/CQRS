35a38533667c3567cf2af4b1396756683eeba18d
Grover <grover@results.com>
2018-03-27 22:34:44 +1300

    Added support for the SQL EventStore to support snapshots.

---
+++ Framework/Cqrs.Tests.Integrations/SnapshotTests.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cdmdotnet.Logging;
+using cdmdotnet.StateManagement.Threaded;
+using Cqrs.Authentication;
+using Cqrs.Configuration;
+using Cqrs.Domain;
+using Cqrs.Domain.Factories;
+using Cqrs.Events;
+using Cqrs.Messages;
+using Cqrs.Ninject.Configuration;
+using Cqrs.Snapshots;
+using Cqrs.Tests.Integrations.Configuration;
+using NUnit.Framework;
+
+namespace Cqrs.Tests.Integrations
+{
+	[TestFixture]
+	public class SnapshotTests
+	{
+		[Test]
+		public void Should_load_events()
+		{
+			// Arrange
+			NinjectDependencyResolver.ModulesToLoad.Add(new CqrsModule<int, DefaultAuthenticationTokenHelper>());
+			NinjectDependencyResolver.ModulesToLoad.Add(new SimplifiedSqlModule<int>());
+			NinjectDependencyResolver.ModulesToLoad.Add(new InProcessEventBusModule<int>());
+			NinjectDependencyResolver.ModulesToLoad.Add(new TestCqrsModule());
+			NinjectDependencyResolver.Start();
+			var snapshotStore = DependencyResolver.Current.Resolve<ISnapshotStore>();
+			var snapshotStrategy = DependencyResolver.Current.Resolve<ISnapshotStrategy<int>>();
+			var unitOfWork = new UnitOfWork<int>(new SnapshotRepository<int>
+			(
+				snapshotStore,
+				snapshotStrategy,
+				new AggregateRepository<int>
+				(
+					DependencyResolver.Current.Resolve<IAggregateFactory>(),
+					DependencyResolver.Current.Resolve<IEventStore<int>>(),
+					DependencyResolver.Current.Resolve<IEventPublisher<int>>(),
+					new CorrelationIdHelper(new ThreadedContextItemCollectionFactory()),
+					DependencyResolver.Current.Resolve<IConfigurationManager>()
+				),
+				DependencyResolver.Current.Resolve<IEventStore<int>>(),
+				DependencyResolver.Current.Resolve<IAggregateFactory>()
+			));
+			var aggregate = DependencyResolver.Current.Resolve<IAggregateFactory>().Create<TestAggregate>(Guid.NewGuid());
+			unitOfWork.Add(aggregate);
+			int count = 0;
+			do
+			{
+				aggregate.GenerateRandomNumber();
+				if (count%10 == 0)
+				{
+					unitOfWork.Commit();
+					unitOfWork.Add(aggregate);
+				}
+			} while (count++ <= 20);
+			unitOfWork.Commit();
+
+			// Act
+			var aggregate2 = unitOfWork.Get<TestAggregate>(aggregate.Rsn);
+		}
+
+		public class TestAggregate
+			: SnapshotAggregateRoot<int, TestAggregateSnapshot>
+		{
+			/// <summary>
+			/// Gets or sets the <see cref="IDependencyResolver"/> used.
+			/// </summary>
+			protected IDependencyResolver DependencyResolver { get; private set; }
+
+			/// <summary>
+			/// Gets or sets the <see cref="ILogger"/> used.
+			/// </summary>
+			protected ILogger Logger { get; private set; }
+
+			/// <summary>
+			/// Instantiates a new instance of <see cref="AggregateFactory"/>.
+			/// </summary>
+			public TestAggregate(IDependencyResolver dependencyResolver, ILogger logger, Guid rsn)
+			{
+				DependencyResolver = dependencyResolver;
+				Logger = logger;
+				Rsn = rsn;
+			}
+
+			public Guid Rsn
+			{
+				get { return Id; }
+				set { Id = value; }
+			}
+
+			public int CurrentRandomNumber { get; set; }
+
+			public void GenerateRandomNumber()
+			{
+				ApplyChange(new RandomNumberEvent(Rsn));
+			}
+
+			private void Apply(RandomNumberEvent @event)
+			{
+				CurrentRandomNumber = @event.RandomNumber;
+			}
+
+			#region Overrides of SnapshotAggregateRoot<int,TestAggregateSnapshot>
+
+			/// <summary>
+			/// Create a <see cref="TestAggregateSnapshot"/> of the current state of this instance.
+			/// </summary>
+			protected override TestAggregateSnapshot CreateSnapshot()
+			{
+				return new TestAggregateSnapshot {CurrentRandomNumber = CurrentRandomNumber};
+			}
+
+			/// <summary>
+			/// Rehydrate this instance from the provided <paramref name="snapshot"/>.
+			/// </summary>
+			/// <param name="snapshot">The <see cref="TestAggregateSnapshot"/> to rehydrate this instance from.</param>
+			protected override void RestoreFromSnapshot(TestAggregateSnapshot snapshot)
+			{
+				CurrentRandomNumber = snapshot.CurrentRandomNumber;
+			}
+
+			#endregion
+		}
+
+		public class TestAggregateSnapshot
+			: Snapshot
+		{
+			public int CurrentRandomNumber { get; set; }
+		}
+
+		public class RandomNumberEvent
+			: IEventWithIdentity<int>
+		{
+			#region Implementation of IMessage
+
+			/// <summary>
+			/// An identifier used to group together several <see cref="IMessage"/>. Any <see cref="IMessage"/> with the same <see cref="CorrelationId"/> were triggered by the same initiating request.
+			/// </summary>
+			public Guid CorrelationId { get; set; }
+
+			/// <summary>
+			/// The originating framework this message was sent from.
+			/// </summary>
+			public string OriginatingFramework { get; set; }
+
+			/// <summary>
+			/// The frameworks this <see cref="IMessage"/> has been delivered to/sent via already.
+			/// </summary>
+			public IEnumerable<string> Frameworks { get; set; }
+
+			#endregion
+
+			#region Implementation of IMessageWithAuthenticationToken<int>
+
+			/// <summary>
+			/// The <typeparamref name="TAuthenticationToken"/> of the entity that triggered the event to be raised.
+			/// </summary>
+			public int AuthenticationToken { get; set; }
+
+			#endregion
+
+			#region Implementation of IEvent<int>
+
+			/// <summary>
+			/// The ID of the <see cref="IEvent{TAuthenticationToken}"/>
+			/// </summary>
+			public Guid Id { get; set; }
+
+			/// <summary>
+			/// The version of the <see cref="IEvent{TAuthenticationToken}"/>
+			/// </summary>
+			public int Version { get; set; }
+
+			/// <summary>
+			/// The date and time the event was raised or published.
+			/// </summary>
+			public DateTimeOffset TimeStamp { get; set; }
+
+			#endregion
+
+			#region Implementation of IEventWithIdentity<int>
+
+			/// <summary>
+			/// The identity of the <see cref="IAggregateRoot{TAuthenticationToken}">aggregate</see> being targeted.
+			/// </summary>
+			public Guid Rsn { get; set; }
+
+			#endregion
+
+			public int RandomNumber { get; set; }
+
+			public RandomNumberEvent(Guid rsn)
+			{
+				Id = Guid.NewGuid();
+				Rsn = rsn;
+				RandomNumber = new Random().Next(0, 99999);
+			}
+		}
+	}
+}
