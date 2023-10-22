182669bfaec06153bdc8ce453dffbfd5b41decc8
Greg Yo <greyoy@users.noreply.github.com>
2023-10-22 22:16:16 +1300

    Minor test fixes and ninject updates

---
+++ Framework/Ninject/Azure/Cqrs.Ninject.Azure.Storage/Configuration/TableStorageDataStoreModule.cs
@@ -0,0 +1,41 @@
+﻿#region Copyright
+// // -----------------------------------------------------------------------
+// // <copyright company="Chinchilla Software Limited">
+// // 	Copyright Chinchilla Software Limited. All rights reserved.
+// // </copyright>
+// // -----------------------------------------------------------------------
+#endregion
+
+using Cqrs.Azure.Storage.DataStores;
+using Ninject.Modules;
+
+namespace Cqrs.Ninject.Azure.Storage.Configuration
+{
+	/// <summary>
+	/// A <see cref="INinjectModule"/> that wires up <see cref="TableStorageDataStoreConnectionStringFactory"/> as the <see cref="ITableStorageDataStoreConnectionStringFactory"/>.
+	/// </summary>
+	public class TableStorageDataStoreModule : NinjectModule
+	{
+		#region Overrides of NinjectModule
+
+		/// <summary>
+		/// Loads the module into the kernel.
+		/// </summary>
+		public override void Load()
+		{
+			RegisterFactories();
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Register the all factories
+		/// </summary>
+		public virtual void RegisterFactories()
+		{
+			Bind<ITableStorageDataStoreConnectionStringFactory>()
+				.To<TableStorageDataStoreConnectionStringFactory>()
+				.InSingletonScope();
+		}
+	}
+}
