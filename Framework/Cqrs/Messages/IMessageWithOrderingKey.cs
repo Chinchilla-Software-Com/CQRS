6315f9fa9027f5f693dbad7bc9f317146968261a
Grover <135006+cdmdotnet@users.noreply.github.com>
2024-09-01 21:34:31 +1200

    Missing file check-in

---
+++ Framework/Cqrs/Messages/IMessageWithOrderingKey.cs
@@ -0,0 +1,25 @@
+#region Copyright
+// // -----------------------------------------------------------------------
+// // <copyright company="Chinchilla Software Limited">
+// // 	Copyright Chinchilla Software Limited. All rights reserved.
+// // </copyright>
+// // -----------------------------------------------------------------------
+#endregion
+
+using System;
+using System.Collections.Generic;
+
+namespace Cqrs.Messages
+{
+	/// <summary>
+	/// A message such as an event or command with a key to aid in ordering where supported.
+	/// </summary>
+	public interface IMessageWithOrderingKey
+	{
+		/// <summary>
+		/// The key to use to aid in ordering.
+		/// Usually this would be a combination of entity type and entity id. When combined this creates a partition for ordering of messages to occur.
+		/// </summary>
+		string OrderKey { get; set; }
+	}
+}
